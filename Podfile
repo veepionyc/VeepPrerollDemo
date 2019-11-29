4c39dc0ac5020881ac04594814d3628828175ef2
foundry <foundry.tv@gmail.com>
2019-11-29 05:07:41 +0000

    first commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+source 'https://github.com/CocoaPods/Specs.git'
+source 'https://github.com/veepionyc/Specs.git'
+
+platform :ios, '9.0'
+
+use_frameworks!
+
+target "TestAdView" do
+pod 'GoogleAds-IMA-iOS-SDK', '~> 3.9'
+
+#pod 'VPKit'
+pod 'VPKit/Core'  , :path => '../VPKit_7'
+
+end
---
+++ TestAdView/AppDelegate.swift
@@ -0,0 +1,56 @@
+//
+//  AppDelegate.swift
+//  TestAdView
+//
+//  Created by jonathan on 27/11/2019.
+//  Copyright © 2019 Foundry. All rights reserved.
+//
+
+import UIKit
+import VPKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        /*
+         
+         APP CREDENTIALS: required
+         
+         These app credentials are for testing purposes only.
+         To obtain credentials unique to your app visit the VEEPIO developer portal
+         https://developer.veep.io
+         */
+        
+        
+        let appId = "VEEPIO_by_url_test_app_id"
+        let clientId = "1zArpBErovQ1MjVHvigJqXwE8qt47U2Yy5XzG3CP"
+        let clientSecret = "VpLIvEetceUnHBEIf6fLUwLxELBh2QesZ6iLLiPHCesRLXfOLLJNcFfmp03wJfGaJquO3V8KqHjtvzlufuXfWWgcpWVw9wxfBJNYdZh96JHV5hk44dJbqiCqplrKcSml"
+        
+        /*
+         
+         INITIALISATION
+         
+         */
+        
+        VPKit.setApplicationId(appId,
+                               clientId: clientId,
+                               clientSecret: clientSecret)
+
+
+
+ 
+ 
+        return true
+    }
+
+  
+
+
+}
+
